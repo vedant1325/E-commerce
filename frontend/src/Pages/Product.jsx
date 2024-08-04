57c0b3eb57879c72f77e7342ef1a35bb47653116
vedant1325 <147478924+vedant1325@users.noreply.github.com>
2024-08-04 11:40:54 +0530

    Update Product.jsx

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -4,13 +4,13 @@ import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../Context/ShopContext';
 import Breadcrums from '../Components/Breadcrums/Breadcrums';
-import all_product from '../Components/Assets/all_product';
+//import all_product from '../Components/Assets/all_product';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import Descriptionbox from '../Components/Descriptionbox/Descriptionbox';
 import RelatedProduct from '../Components/RelatedProduct/RelatedProduct';
 
 const Product = () => {
-  const{all_product}=useContext(ShopContext);
+ // const{all_product}=useContext(ShopContext);
   const {productId}=useParams();
   const product = all_product.find((e)=>e.id===Number(productId))
   return (
